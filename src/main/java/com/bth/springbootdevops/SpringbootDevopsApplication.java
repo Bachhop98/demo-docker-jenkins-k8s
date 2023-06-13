d31f23e48260f4e7c36ddb6ac4b786d66d5f174f
bachhop98 <bachhop9880@gmail.com>
2023-06-13 12:12:37 +0700

    update code

---
+++ src/main/java/com/bth/springbootdevops/SpringbootDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.bth.springbootdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDevopsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/bth/springbootdevops/controller/MailController.java
@@ -0,0 +1,8 @@
+package com.bth.springbootdevops.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class MailController {
+}
